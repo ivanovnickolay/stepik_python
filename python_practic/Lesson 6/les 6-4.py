1cb5d6d614fe397a38cb0d6835857107453c7afc
ivanov.nickolay <ivanov.nickolay@gmail.com>
2020-05-28 18:25:50 +0300

    fixup! Lesson 6 part 2

---
+++ python_practic/Lesson 6/les 6-4.py
@@ -0,0 +1,24 @@
+import cgi
+
+def ohash(s):
+    ans = 0
+    for c in s:
+        ans = ans * 123417 + ord(c)
+    return ans
+
+# form = cgi.FieldStorage()
+# text = form.getfirst("INPUT_TEXT", "не задано")
+text = "Python"
+
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>""")
+
+print("<h1>" + str(ohash(text)) + "</h1>")
+
+print("""</body>
+        </html>""")
