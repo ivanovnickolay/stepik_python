f7377ddbe1ddfd6e3c0afe95043dcc721ac70097
ivanov.nickolay <ivanov.nickolay@gmail.com>
2020-07-15 00:54:19 +0300

    selenium. запуск автотестов для разных языков интерфейса

---
+++ Python_selenium/Lesson 3/less 3-6/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Languare...")
+
+
+@pytest.fixture(scope="function")
+def browser(request) -> webdriver:
+    browser_name = request.config.getoption("browser_name")
+    languare_name = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': languare_name})
+        browser = webdriver.Chrome('../ChromeDriver/chromedriver.exe',chrome_options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': languare_name})
+        browser = webdriver.Chrome('../ChromeDriver/chromedriver.exe', chrome_options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+# @pytest.fixture(scope="function")
+# def language_name(request):
+#     languare_name = request.config.getoption("language")
+#     return languare_name
+
---
+++ Python_selenium/Lesson 3/less 3-6/test_items.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+
+def test_button(browser: webdriver):
+    """
+        Тестируем наличие кнопки на странице
+    :param browser: webdriver
+    :return:
+    """
+    def isElementPresent() -> bool:
+        """
+        Проверяем наличие элемента, если него нет на странице то "ловим" исключение
+        и обрабатываем его
+        :rtype: bool
+            - True - элемент найден
+            - False - элемент не найден
+        """
+        try:
+            browser.find_elements_by_class_name('btn btn-lg btn-primary btn-add-to-basket')
+            return True
+        except NoSuchElementException:
+            return False
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(10)
+    assert isElementPresent()==True, "Button not found"
