47ec2f625e4bcb720347c281ba8ce5514b965682
ivanov.nickolay <ivanov.nickolay@gmail.com>
2020-07-18 00:16:55 +0300

    selenium. Lesson 4 Page Object. add def is_element_present

--- Python_selenium/Lesson_4/pages/base_page.py
+++ Python_selenium/Lesson_4/pages/base_page.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url,  timeout=10):
         self.browser: webdriver = browser
         self.url: str = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
